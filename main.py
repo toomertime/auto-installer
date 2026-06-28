3169daa47079839143c522db518eaa68bce52c19
toomertime <atoomer@proton.me>
2026-06-28 19:09:05 -0400

    swapped select all and deselect all buttons for a checkbox

--- main.py
+++ main.py
@@ -10,29 +10,25 @@ class App:
         self.root.geometry("400x300")
 
         self.vars = []
+        self.select_all_var = tk.BooleanVar()
+        select_all_chk = tk.Checkbutton(self.root, text="Select All", variable=self.select_all_var, anchor="w", command=self.toggle_all)
+        select_all_chk.pack(fill="x")
+        tk.Label(self.root, text="").pack()
 
         for app in APPS:
             var = tk.BooleanVar()
             checkbox = tk.Checkbutton(self.root, text=app["name"], variable=var, anchor="w")
             checkbox.pack(fill="x")
             self.vars.append(var)
-            
+
         install_btn = tk.Button(self.root, text="Install", command=self.start_install)
         install_btn.pack()
-        select_all_btn = tk.Button(self.root, text="Select All", command=self.select_all)
-        select_all_btn.pack()
-        deselect_all_btn = tk.Button(self.root, text="Deselect All", command=self.deselect_all)
-        deselect_all_btn.pack()
         self.status_label = tk.Label(self.root, text="Ready")
         self.status_label.pack()
 
-    def select_all(self):
+    def toggle_all(self):
         for var in self.vars:
-            var.set(True)
-
-    def deselect_all(self):
-        for var in self.vars:
-            var.set(False)
+            var.set(self.select_all_var.get())
 
     def on_install(self):
         for app, var in zip(APPS, self.vars):
