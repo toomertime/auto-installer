158c3f068adbe341f65c19564a8f27f29992ddbe
toomertime <atoomer@proton.me>
2026-06-29 19:08:39 -0400

    added install_location key for Battle.net in catalog. Changed logic to build command list dynamically to handle install_location

--- installer.py
+++ installer.py
@@ -1,14 +1,18 @@
 import subprocess
 
 def install_app(app):
-    # Run the winget install command for the given winget ID
-    subprocess.run([
+    cmd = [
         "winget", "install",
         "--silent", 
         "--accept-package-agreements",
         "--accept-source-agreements", 
         app["winget_id"]
-    ])
+    ]
+    if "install_location" in app:
+        cmd += ["--location", app["install_location"]]
+
+    subprocess.run(cmd)
+    
     # iff app has a process_name, attempt to kill it after installation
     if "process_name" in app:
         subprocess.run(["taskkill", "/f", "/im", app["process_name"]])
